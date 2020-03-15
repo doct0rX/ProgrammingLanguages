061adee20c1df70ea60efebe26309de0bfaf81a8
MustafaJ4m <mustafaj4m@gmail.com>
2020-03-16 01:28:53 +0200

    hw6 done

--- PartC/section8/hw6/hw6assignment.rb
+++ PartC/section8/hw6/hw6assignment.rb
@@ -5,35 +5,68 @@
 
 class MyPiece < Piece
     # The constant All_My_Pieces holds the all 10 pieces
-    All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-                    rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-                    [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-                    [[0, 0], [0, -1], [0, 1], [0, 2]]],
-                    rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-                    rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-                    rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-                    rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
-                    rotations([[0, 0], [0, 1], [1, 1], [0, 0]]),    # small L
-                    [[[0, 0], [-1, 0], [1, 0], [2, 0], [3, 0]], # Long bar (only needs 2)
-                    [[0, 0], [0, -1], [0, 1], [0, 2], [0, 3]]],
-                    rotations([[0, 0], [0, 1], [1, 1], [-1, 0], [-1, 1]])]  # square w/ extension
+    All_My_Pieces = All_Pieces +
+                    [rotations([[0, 0], [-1, 0], [-1, -1], [0, -1], [1, 0]]),
+                    [[[0, 0], [-1, 0], [-2, 0], [1, 0], [2, 0]],
+                    [[0, 0], [0, -1], [0, -2], [0, 1], [0, 2]]],
+                    rotations([[0, 0], [0, -1], [1, 0]])]
 
-    # Cheat_Piece = [[0,0]] # just one square box
+    Cheat_Piece = [[[0, 0]]] # just one square box
 
     # My enhancements
-    
-  
+    def self.next_piece(board)
+        MyPiece.new(All_My_Pieces.sample, board)
+    end
+
+    def self.next_cheat_piece(board)
+        MyPiece.new(Cheat_Piece, board)
+    end
   end
   
   class MyBoard < Board
     # your enhancements here
-    
-    def rotate_oneEighty
+    def initialize (game)
+        @grid = Array.new(num_rows) {Array.new(num_columns)}
+        @current_block = MyPiece.next_piece(self)
+        @score = 0
+        @game = game
+        @delay = 500
+        @cheat = false
+    end
+
+    def rotate_180
         if !game_over? and @game.is_running?
-          @current_block.move(0, 0, 1)
-          @current_block.move(0, 0, 1)
+          @current_block.move(0, 0, 2)
         end
         draw
+    end
+
+    def init_cheat
+        if !@cheat and @score >= 100
+            @cheat = true
+            @score -= 100
+        end
+    end
+
+    def next_piece
+        if @cheat
+            @current_block = MyPiece.next_cheat_piece(self)
+            @cheat = false
+        else
+            @current_block = MyPiece.next_piece(self)
+        end
+        @current_pos = nil
+    end
+
+    def store_current
+        locations = @current_block.current_rotation
+        displacement = @current_block.position
+        (0..(locations.size - 1)).each{|index| 
+            current = locations[index];
+            @grid[current[1]+displacement[1]][current[0]+displacement[0]] = @current_pos[index]
+        }
+        remove_filled
+        @delay = [@delay - 2, 80].max
     end
   end
   
@@ -43,20 +76,21 @@ class MyPiece < Piece
         @canvas = TetrisCanvas.new
         @board = MyBoard.new(self)
         @canvas.place(@board.block_size * @board.num_rows + 3,
-        @board.block_size * @board.num_columns + 6, 24, 80)
+                    @board.block_size * @board.num_columns + 6, 24, 80)
         @board.draw
     end
-    # 1. 'U' to rotate 180 degree
     def key_bindings
         super
-        @root.bind('u', proc {@board.rotate_oneEighty})
+        @root.bind('u', proc {@board.rotate_180})
+        @root.bind('c', proc {@board.init_cheat})
     end
     def buttons
         super
-        rotate_one = TetrisButton.new('(_.', 'lightgreen'){@board.rotate_oneEighty}
+        rotate_one = TetrisButton.new('(_.', 'lightgreen'){@board.rotate_180}
         rotate_one.place(35, 50, 27, 501)
+
+        cheat_btn = TetrisButton.new('C', 'lightgreen'){@board.init_cheat}
+        cheat_btn.place(35, 50, 127, 571)
     end
-  
   end
-  
   

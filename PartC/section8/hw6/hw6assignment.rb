c598edb2c2d775e45f5a04ed12347aa731ce1eb3
MustafaJ4m <mustafaj4m@gmail.com>
2020-03-16 00:19:12 +0200

    make 1st enhancement for the game rotating 180 degree

--- PartC/section8/hw6/hw6assignment.rb
+++ PartC/section8/hw6/hw6assignment.rb
@@ -4,19 +4,58 @@
 # part of your solution.
 
 class MyPiece < Piece
-    # The constant All_My_Pieces should be declared here
-  
-    # your enhancements here
+    # The constant All_My_Pieces holds the all 10 pieces
+    All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+                    rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+                    [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+                    [[0, 0], [0, -1], [0, 1], [0, 2]]],
+                    rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+                    rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+                    rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+                    rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
+                    rotations([[0, 0], [0, 1], [1, 1], [0, 0]]),    # small L
+                    [[[0, 0], [-1, 0], [1, 0], [2, 0], [3, 0]], # Long bar (only needs 2)
+                    [[0, 0], [0, -1], [0, 1], [0, 2], [0, 3]]],
+                    rotations([[0, 0], [0, 1], [1, 1], [-1, 0], [-1, 1]])]  # square w/ extension
+
+    # Cheat_Piece = [[0,0]] # just one square box
+
+    # My enhancements
+    
   
   end
   
   class MyBoard < Board
     # your enhancements here
-  
+    
+    def rotate_oneEighty
+        if !game_over? and @game.is_running?
+          @current_block.move(0, 0, 1)
+          @current_block.move(0, 0, 1)
+        end
+        draw
+    end
   end
   
   class MyTetris < Tetris
-    # your enhancements here
+    # My enhancements here
+    def set_board
+        @canvas = TetrisCanvas.new
+        @board = MyBoard.new(self)
+        @canvas.place(@board.block_size * @board.num_rows + 3,
+        @board.block_size * @board.num_columns + 6, 24, 80)
+        @board.draw
+    end
+    # 1. 'U' to rotate 180 degree
+    def key_bindings
+        super
+        @root.bind('u', proc {@board.rotate_oneEighty})
+    end
+    def buttons
+        super
+        rotate_one = TetrisButton.new('(_.', 'lightgreen'){@board.rotate_oneEighty}
+        rotate_one.place(35, 50, 27, 501)
+    end
   
   end
   
